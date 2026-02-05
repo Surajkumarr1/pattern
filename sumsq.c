dbda6ac5d15f30f71988e678b9371501859599ab
suraj  kumar <singhsurajsi905@gmail.com>
2026-02-05 15:12:09 +0530

    Add sumsq.c to calculate sum of squares
    Implement a program to calculate the sum of squares up to n.

---
+++ sumsq.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int a=1;
+    for(int i=0;i<=n;i++){
+        
+        a+=i*i;
+        return a;
+       
+    }
+    return 0;
+}
+
+half solved ....................... 
